bd5c2cb2b3066bab7a846a28d9350a1c25927b6e
alireza-hb <habibialireza38@gmail.com>
2024-06-27 05:48:54 +0430

    fifth

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,9 @@ import medu from "./meeenu/medu";
 import menuu from "./meenu/meenu";
 import menu from "./menu/menu";
 import product_d1 from "./pro.1/pro.1";
+import product_d2 from "./pro.2/pro.2";
+import product_d3 from "./pro.3/pro.3";
+import product_d4 from "./pro.4/pro.4";
 import product from "./product/product";
 import slider from "./slider/slider";
 function initslider() {
@@ -30,6 +33,9 @@ async function allData() {
   await melu();
   await product();
   await product_d1();
+  await product_d2();
+  await product_d3();
+  await product_d4();
   await slider();
   // await product();
   initslider();
